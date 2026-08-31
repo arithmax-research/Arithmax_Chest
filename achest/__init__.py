c68547938f0db933e96a38d4db672fa22202e7c2
FranklineMisango <franklinemisango@gmail.com>
2026-08-31 23:54:16 +0800

    bump version to 0.4.0 for Lean format support

--- achest/__init__.py
+++ achest/__init__.py
@@ -3,6 +3,6 @@
 from .client import MarketDataClient
 from .service import to_lean_zip, to_q_table
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = ["MarketDataClient", "to_lean_zip", "to_q_table", "__version__"]
